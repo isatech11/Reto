b19a33f04330acba55ade795e7ffd0e7dbc360b9
Isabel Morales <isabel.moralesp@hotmail.com>
2022-01-28 17:50:45 -0500

    retos

---
+++ reto.js
@@ -0,0 +1,19 @@
+const ovejas = [
+    { name: 'Noa', color: 'azul' },
+    { name: 'Euge', color: 'rojo' },
+    { name: 'Navidad', color: 'rojo' },
+    { name: 'Ki Na Ma', color: 'rojo'},
+    { name: 'AAAAAaaaaa', color: 'rojo' },
+    { name: 'Nnnnnnnn', color: 'rojo'}
+  ]
+
+  
+    const ovejasFiltradas = contarOvejas(ovejas)
+
+console.log(ovejasFiltradas)
+
+// [{ name: 'Navidad', color: 'rojo' },
+//  { name: 'Ki Na Ma', color: 'rojo' }]
+
+export default function contarOvejas(ovejas) {
+    return ovejas.filter((oveja) => oveja.color === 'rojo' && oveja.name.indexOf("Na") !== -1)
